b2ee029197c7b2a4899f6c0a2afede9b0565cff7
vtrfaas <vitor.f.arantes@gmail.com>
2016-04-28 15:32:28 -0300

    Criado a entidade cliente

---
+++ LivrariaDigital/src/entity/Cliente.java
@@ -0,0 +1,50 @@
+package entity;
+
+import java.util.Date;
+
+public class Cliente {
+	
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String cpf;
+	private String dtNasc;
+	private int idade;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getDtNasc() {
+		return dtNasc;
+	}
+	public void setDtNasc(String dtNasc) {
+		this.dtNasc = dtNasc;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+}
